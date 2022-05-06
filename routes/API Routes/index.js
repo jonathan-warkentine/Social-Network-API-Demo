07ab88f94165cbc4f2f0286f5a993b433a8436c4
Jonathan Warkentine <jonathan.warkentine@gmail.com>
2022-05-06 16:07:36 -0400

    working on models

--- routes/API Routes/index.js
+++ routes/API Routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./users.routes');
 const thoughtRoutes = require('./thoughts.routes');
 
-
 router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
